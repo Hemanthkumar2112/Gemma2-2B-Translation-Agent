9818286e6263af836c7fbabb77d4d4605e7e3c59
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2024-10-02 15:29:43 +0530

    docker file changes in pytorch

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,48 @@
-# Use the official Python image as the base image
-FROM python:3.10
-
-RUN apt-get update && apt-get install -y \
-    curl \
-    build-essential
-
-# # Install Poetry
-# RUN curl -sSL https://install.python-poetry.org | python3 -
-# ENV PATH="/root/.local/bin:$PATH"
-WORKDIR /indic_translate
-
-#COPY pyproject.toml poetry.lock* /indic_translate/
-
-#RUN poetry install --no-root
-
-COPY requirements.txt . 
-RUN pip install --no-cache-dir -r requirements.txt
  
-COPY . .
-EXPOSE 8080
+# FROM python:3.10
 
+# RUN apt-get update && apt-get install -y \
+#     curl \
+#     build-essential
+
+ 
+# WORKDIR /indic_translate
+
+ 
+
+# COPY requirements.txt . 
+# RUN pip install --no-cache-dir -r requirements.txt
+ 
+# COPY . .
+# EXPOSE 8080
+
+# CMD ["streamlit", "run", "app.py", "--server.port=8080", "--server.address=0.0.0.0"]
+
+
+# Base image with CUDA support
+FROM pytorch/pytorch:2.0.1-cuda11.7-cudnn8-runtime
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    git \
+    python3-pip \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install the correct version of bitsandbytes with CUDA support
+RUN pip install bitsandbytes-cuda117
+
+# Install Hugging Face Transformers and other dependencies
+RUN pip install transformers torch torchvision --upgrade
+
+# Set the working directory
+WORKDIR /app
+
+
+# Copy the project files into the container
+COPY . /app
+
+# Install additional Python dependencies if needed
+RUN pip install -r requirements.txt
+
+# Run the Hugging Face model script
 CMD ["streamlit", "run", "app.py", "--server.port=8080", "--server.address=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,32 +1,42 @@
 import streamlit as st
-import time
+from indic_MT_inference_script import peft_model,adapter_names,generate
 st.title("_Indic Machine translation with Gemma2 2B_  :sunglasses:",)
 
 # Dictionary of languages with descriptions and their respective language codes
 languages = {
-    "Tamil 🇮🇳": {"code": "ta" },
-    "Hindi 🇮🇳": {"code": "hi" },
-    "Kannada 🇮🇳": {"code": "kn",  },
-    "Malayalam 🇮🇳": {"code": "ml",  },
-    "Telugu 🇮🇳": {"code": "te",  },
-    "Bengali 🇮🇳": {"code": "bn",  },
-    "Marathi 🇮🇳": {"code": "mr", },
-    "Gujarati 🇮🇳": {"code": "gu", },
-    "Odia 🇮🇳": {"code": "or", }
+    "Tamil 🇮🇳": {"code": "ta","lang":"tamil" } ,
+    "Hindi 🇮🇳": {"code": "hi","lang": "hindi"},
+    "Kannada 🇮🇳": {"code": "kn","lang":"kannada"},
+    "Malayalam 🇮🇳": {"code": "ml", "lang": "malayalam"},
+    "Telugu 🇮🇳": {"code": "te","lang": "telugu" },
+    "Bengali 🇮🇳": {"code": "bn",  "lang":"bengali"},
+    "Marathi 🇮🇳": {"code": "mr","lang": "marathi"},
+    "Gujarati 🇮🇳": {"code": "gu", "lang":"gujarati"},
+    "Odia 🇮🇳": {"code": "or", "lang":"odia"}
 }
 
 col1, col2,col3 = st.columns(3)
  
 selected_language = col3.radio("Select a language for translation", list(languages.keys()))
 
+### testing # lang,english_text,response_text,merged_model
+#text = "Heavy rain has caused widespread flooding in several parts of the country."
+
+
+def translate_text(lang,text):
+    inference_result = generate(lang,text,"",peft_model,to_set_lang=True)
+    print(f"inference_result for {lang} : {inference_result}")
+    return inference_result
  
 with col1:
     text = col1.text_area("English")
     if col1.button("Translate"):
         if text:
             with st.spinner("Translating..."):
-                time.sleep(5)
-                st.session_state['translated_text'] = text
+                print(selected_language)
+                print(languages[selected_language]['code'],languages[selected_language]['lang'])
+                translated_result = translate_text(text=text,lang=languages[selected_language]['lang'])
+                st.session_state['translated_text'] = translated_result
         else:
             st.warning('please enter any english sentence to translate', icon="⚠️")
  
